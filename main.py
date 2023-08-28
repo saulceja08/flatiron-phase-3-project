62a3a6f85ca06470a34e7e2503b7d7fbf435752c
Saul Ceja <saulceja08@gmail.com>
2023-08-27 22:59:14 -0700

    Define the User class that inherits from the base class and added the attributes for the users table

---
+++ main.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+#define the base class 
+Base = declarative_base() 
+
+#Define the User class that will inherti from the base class -saulceja
+class User(Base):
+    __tablename__ = "users"
+
+    member_id = Column(Integer, primary_key=True, unique=True)
+    usernname = Column(String(25), unique=True, nullable=False)
+    first_name = Column(String(25), unique=False, nullable=False)
+    last_name = Column(String(25), unique=False, nullable=False)
+    birth_date = Column(DateTime(), default=datetime.utcnow)
+    email = Column(String(25), unique=True, nullable=False)
