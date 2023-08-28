fbe7e4db8a1a256d0c64bd977c30dccb741f7fe8
Saul Ceja <saulceja08@gmail.com>
2023-08-27 23:40:56 -0700

    defined the __init__ and __repr__ methodto initialize and print user instances

--- main.py
+++ main.py
@@ -16,4 +16,22 @@ class User(Base):
     last_name = Column(String(25), unique=False, nullable=False)
     birth_date = Column(DateTime())
     email = Column(String(25), unique=True,nullable=False)
+    
+    def __init__(self, member_id, username, first_name, last_name, birth_date, email):
+        self.member_id = member_id\
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
+        self.email = email
 
+        def __init__(self, member_id, username, first_name, last_name, birth_date, email):
+    self.member_id = member_id
+    self.username = username
+    self.first_name = first_name
+    self.last_name = last_name
+    self.birth_date = birth_date
+    self.email = email
+
+    def __repr__(self):
+        return f"{self.member_id}{self.username}{self.first_name}{self.last_name}{self.birth_date}{self.email}"
