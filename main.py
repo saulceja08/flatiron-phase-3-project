88f8dde7c10e32b997e89ae464fe4e5ebe60f532
Saul Ceja <saulceja08@gmail.com>
2023-08-28 20:14:12 -0700

    Adjusted the datetime format when passing the attribute along with debugging User class __init__

--- main.py
+++ main.py
@@ -11,31 +11,30 @@ class User(Base):
     __tablename__ = "users"
 
     member_id = Column(Integer, primary_key=True, unique=True)
-    usernname = Column(String(25), unique=True, nullable=False)
+    username = Column(String(25), unique=True, nullable=False)
     first_name = Column(String(25), unique=False, nullable=False)
     last_name = Column(String(25), unique=False, nullable=False)
     birth_date = Column(DateTime())
     email = Column(String(45), unique=True,nullable=False)
     
-    def __init__(self, member_id, username, first_name, last_name, birth_date, email):
-        self.member_id = member_id
+    def __init__(self, username, first_name, last_name, birth_date, email):
         self.username = username
         self.first_name = first_name
         self.last_name = last_name
-        self.birth_date = birth_date
+        self.birth_date = datetime.strptime(birth_date, "%d-%m-%Y")
         self.email = email
 
     def __repr__(self):
-        return f"{self.member_id}{self.username}{self.first_name}{self.last_name}{self.birth_date}{self.email}"
-    
+        return f"<User({self.member_id}, {self.username}, {self.first_name}, {self.last_name}, {self.birth_date}, {self.email})>"
 
 #connection to a database so that we can interact with it (echo logs SQL statements into console)
-engine = create_engine("sqllite:///fitness.db", echo=True)
-base.metadata.create_all(bind=engine)
+engine = create_engine("sqlite:///fitness.db", echo=True)
+Base.metadata.create_all(bind=engine)
 
 #create a session class and object to interact using ORM
 Session = sessionmaker(bind=engine)
 session = Session()
 
-new_user = User(1, "saulceja08", "Saul", "Ceja", 17-1-1999, "saulceja08@gmail.com")
+new_user = User("saulceja08", "Saul", "Ceja", "17-01-1999", "saulceja08@gmail.com")
 session.add(new_user)
+session.commit() #Note: This will commit the changes to the database 
