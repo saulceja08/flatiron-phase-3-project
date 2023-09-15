93ad38d5cdcb8f07697cfdc20ba99b7832c08ff3
Saul Ceja <saulceja08@gmail.com>
2023-09-14 23:23:42 -0700

    removed previous main.py code as it was modified in models.py

--- main.py
+++ main.py
@@ -1,43 +1,9 @@
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
+from app.models import Base
+from sqlalchemy import create_engine
 
-#define the base class using declarative_base
-Base = declarative_base() 
+#Create SQLite db file 
+engine = create_engine('sqlite:///db/fitness.db')
 
-#Define the User class that will inherit from the base class -saulceja
-class User(Base):
-    __tablename__ = "users"
-
-    member_id = Column(Integer, primary_key=True, unique=True)
-    username = Column(String(25), unique=True, nullable=False)
-    first_name = Column(String(25), unique=False, nullable=False)
-    last_name = Column(String(25), unique=False, nullable=False)
-    birth_date = Column(DateTime())
-    email = Column(String(55), unique=True,nullable=False)
-    date_created = Column(DateTime(), default=datetime.utcnow)
-    
-    def __init__(self, username, first_name, last_name, birth_date, email):
-        self.username = username
-        self.first_name = first_name
-        self.last_name = last_name
-        self.birth_date = datetime.strptime(birth_date, "%d-%m-%Y")
-        self.email = email
-        self.date_created = datetime.utcnow()
-
-    def __repr__(self):
-        return f"<User({self.member_id}, {self.username}, {self.first_name}, {self.last_name}, {self.birth_date}, {self.email})>"
-
-#connection to a database so that we can interact with it (echo logs SQL statements into console)
-engine = create_engine("sqlite:///fitness.db", echo=True)
-Base.metadata.create_all(bind=engine)
-
-#create a session class and object to interact using ORM
-Session = sessionmaker(bind=engine)
-session = Session()
-
-new_user = User("saulceja08", "Saul", "Ceja", "17-01-1999", "saulceja08@gmail.com")
-session.add(new_user)
-session.commit() #Note: This will commit the changes to the database  
+#Create the tables from models
+Base.metadata.create_all(engine)
 
