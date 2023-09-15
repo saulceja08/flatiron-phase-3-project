9cdac903c41ccfe50e39fb509dfc949f5a28eb09
Saul Ceja <saulceja08@gmail.com>
2023-09-14 21:15:35 -0700

    new base classes added with defined instances for sqlalchemy tables

--- app/models.py
+++ app/models.py
@@ -1,14 +1,37 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
-Base = declarative_base()
+Base = declarative_base() #Notes: declarative_base() is used to define the main class for creating instances (defien a table)
 
 class User(Base):
-    pass
+    __tablename__ = "users"
+
+    member_id = Column(Integer, primary_key=True, unique=True)
+    username = Column(String(25), unique=True, nullable=False)
+    password = Column(String(30), unique=False, nullable=False)
+    last_name = Column(String(25), unique=False, nullable=True)
+    birth_date = Column(DateTime())
+    email = Column(String(55), unique=True,nullable=False)
+    date_created = Column(DateTime(), default=datetime.utcnow)
+
+    member = relationship("WeightTracker", back_populates="user")
 
 class Workout(Base):
-    pass
+    __tablename__ = 'workouts'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, default=func.now(), nullable=False)
+    duration_minutes = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship('User', back_populates='workouts')
 
 class WeightTracker(Base):
-    pass
+    id = Column(Integer, primary_key=True, unique=True)
+    user_id = Column(Integer, ForeignKey('users.member_id'))
+    current_weight = (Integer)
+    previous_weight = (Integer)
+
+    user = relationship("User", back_populates='member')
