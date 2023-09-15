10ea40ebeabe914571bee0f48a8bc231dcce8b83
Saul Ceja <saulceja08@gmail.com>
2023-09-14 21:23:47 -0700

    Removed original code from main.py as this is now part of models.py

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
-Base = declarative_base() #Notes: declarative_base() is used to define the main class for creating instances (define a table)
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
 
     member_id = Column(Integer, primary_key=True, unique=True)
     username = Column(String(25), unique=True, nullable=False)
-    password = Column(String(30), unique=False, nullable=False)
-    last_name = Column(String(25), unique=False, nullable=True)
+    password = Column(String(30), nullable=False)
+    last_name = Column(String(25), nullable=True)
     birth_date = Column(DateTime())
     email = Column(String(55), unique=True, nullable=False)
     date_created = Column(DateTime(), default=datetime.utcnow)
@@ -30,9 +30,11 @@ class Workout(Base):
     user = relationship('User', back_populates='workouts')
 
 class WeightTracker(Base):
+    __tablename__ = 'weight_tracker'
+
     id = Column(Integer, primary_key=True, unique=True)
     user_id = Column(Integer, ForeignKey('users.member_id'))
     current_weight = Column(Integer)
     previous_weight = Column(Integer)
 
-    user = relationship("User", back_populates='member')
+    user = relationship("User", back_populates='weight_tracker')
