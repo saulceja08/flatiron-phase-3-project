38df09339ce836b734ba1daaba29122b739a938d
Saul Ceja <saulceja08@gmail.com>
2023-09-17 15:29:44 -0700

    Added new class method with click functionality and parameter

---
+++ commands.py/log_weight.py
@@ -0,0 +1,9 @@
+import click
+
+@click.command()
+@click.option('--user_id',promp='User ID',type=int, help='User ID who will own this weight')
+@click.option('--current_weight',prompt='Current Weight(kg)',type=,int help='Weight after workout in kg')
+@click.option('--previous_weight',prompt='Previous Weight(kg)',type=int, help='Weight before workout in kg')
+
+def log_weight(user_id, current_weight, previous_weight):
+    pass
