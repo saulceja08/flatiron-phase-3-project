ab356a4d8c02da3a93f8f6d49b6220805a9f706b
Saul Ceja <saulceja08@gmail.com>
2023-09-17 15:41:50 -0700

    Debug file and corrected typo errors in the method and click sections

--- commands.py/log_weight.py
+++ commands.py/log_weight.py
@@ -1,9 +1,9 @@
 import click
-import app.models import User, WeightTracker, Session
+from app.models import User, WeightTracker, Session
 
 @click.command()
-@click.option('--user_id',promp='User ID',type=int, help='User ID who will own this weight')
-@click.option('--current_weight',prompt='Current Weight(kg)',type=,int help='Weight after workout in kg')
+@click.option('--user_id',prompt='User ID',type=int, help='User ID who will own this weight')
+@click.option('--current_weight',prompt='Current Weight(kg)',type=int, help='Weight after workout in kg')
 @click.option('--previous_weight',prompt='Previous Weight(kg)',type=int, help='Weight before workout in kg')
 
 def log_weight(user_id, current_weight, previous_weight):
@@ -11,7 +11,7 @@ def log_weight(user_id, current_weight, previous_weight):
 
     user = session.query(User).filter_by(member_id=user_id).first()
 
-    if not user():
+    if not user:
         return click.echo('User not found.')
     
     new_weight_calc = WeightTracker(
@@ -21,7 +21,7 @@ def log_weight(user_id, current_weight, previous_weight):
     )
 
     session.add(new_weight_calc)
-    session,commit()
+    session.commit()
 
     click.echo('Weight has been logged successfully.')
 
