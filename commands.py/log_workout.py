fc04d5978c752ad8b8c26391c4ec12810be4a3f5
Saul Ceja <saulceja08@gmail.com>
2023-09-17 14:49:44 -0700

    add click options to get values and pass them to method class

--- commands.py/log_workout.py
+++ commands.py/log_workout.py
@@ -2,9 +2,26 @@ import click
 from app.models import Workout, sessionmaker
 
 @click.command()
-@click.option()
-@click.option()
+@click.option('--user_id', prompt=User ID, type=int, help='User ID who will own this workout')
+@click.option('--duration_minutes', propmt='Duration(minutes)', type=int,help='Total minutes for workout')
 
 def log_workout():
-    pass
+    session = Session()
 
+    user = session.query(User).filter_by(member_id=user_id).first()
+
+    if not user:
+        return click.echo('User not found.')
+    
+    new_workout = Workout(
+        duration_minutes=duration_minutes,
+        user_id=user_id
+    )
+
+    session.add(new_workout)
+    session.commit()
+
+    click.echo('Workout was logged successfully!')
+
+if __name__ == '__main__':
+    log_workout()
