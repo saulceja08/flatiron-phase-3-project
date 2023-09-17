d365430ef11c85ed5dfb6dc647a7ad71ac6250a5
Saul Ceja <saulceja08@gmail.com>
2023-09-17 16:45:03 -0700

    handle click events and make sure user and workout ID exists

--- commands.py/delete_workout.py
+++ commands.py/delete_workout.py
@@ -1,10 +1,17 @@
 import click
-from app.models import Workout, sessionmaker
+from app.models import User, Workout, sessionmaker
 
 @click.command()
 @click.option('--user_id', prompt='User ID', type=int, help='User ID who will own this deletion')
 @click.option('--workout_id',prompt='Workout ID',type=int,help='Worokout ID you would like to delete')
 
 def delete_workout(user_id, workout_id):
-    pass
+    session = Session()
+
+    user = session.query(User).filter_by(member_id=user.id).first()
+
+    if not user:
+        return click.echo('User not found.')
+
+
 
