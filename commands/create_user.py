f7bde682cbf605cef46c402be9052e5ec6b62461
Saul Ceja <saulceja08@gmail.com>
2023-09-17 17:14:18 -0700

    debug file and changed naming convention for commands folder as it was originally a file

--- commands/create_user.py
+++ commands/create_user.py
@@ -5,7 +5,8 @@ from main import Session
 @click.command()
 @click.option('--username', prompt='Username', help='Username to login')
 @click.option('--password', prompt='Password', hide_input=True, confirmation_prompt=True, help='Password to login')
-@click.option('--last_name', prompt='Last Name', help='Username last name')
+@click.option('--first_name', prompt='First Name', help="User first name")
+@click.option('--last_name', prompt='Last Name', help='User last name')
 @click.option('--birth_date', prompt='Birth Date (YYY-MM-DD)', type=click.DateTime(formats=["%Y-%m-%d"]), help='Username birthdate')
 @click.option('--email', prompt='Email', help='Email address for the new user')
 
@@ -22,7 +23,7 @@ def create_user(username, password, first_name, last_name, birth_date, email):
     )
 
     session.add(new_user)
-    session.commi()
+    session.commit()
 
     click.echo('User successfully created!')
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.models import Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 #Create SQLite db file 
 engine = create_engine('sqlite:///db/fitness.db', echo=True)
@@ -7,6 +8,7 @@ engine = create_engine('sqlite:///db/fitness.db', echo=True)
 #Create all tables from models
 Base.metadata.create_all(bind=engine)
 
+#Create a session 
 Session = sessionmaker(bind=engine)
 
 session = Session()
