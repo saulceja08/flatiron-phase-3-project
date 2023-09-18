71ee4e82a496494afb94bd351a3edb3aded72113
Saul Ceja <saulceja08@gmail.com>
2023-09-17 17:23:32 -0700

    db changed to database in the engine path as db is not the original name of the file.

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 #Create SQLite db file 
-engine = create_engine('sqlite:///db/fitness.db', echo=True)
+engine = create_engine('sqlite:///database/fitness.db', echo=True)
 
 #Create all tables from models
 Base.metadata.create_all(bind=engine)
 
 #Create a session 
 Session = sessionmaker(bind=engine)
-
 session = Session()
